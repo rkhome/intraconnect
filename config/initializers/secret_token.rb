c3b4cc5ee5acf106d8ea292318232dbb1ffd0eb5
Raghvendra <raghvendra@ubuntu.ubuntu-domain>
2012-07-08 14:05:20 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Intraconnect::Application.config.secret_token = 'ba4b0ac8b4ae11753f8347ed417d4526d91937f3dd0b37f5d6d51554dae269f4bf32568a5e6f4d82151b2a3e35148d7052376959f654d463b290edcb79ae5e23'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Intraconnect::Application.config.session_store :cookie_store, key: '_intraconnect_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Intraconnect::Application.config.session_store :active_record_store
